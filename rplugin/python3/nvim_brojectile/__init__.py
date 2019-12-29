4d8d992d91d9cb0a77213e2b70d45ff3b39c0a36
jwiersma <info@hossel.net>
2019-12-29 15:07:05 +0100

    Restructure code
    Neovim functions/commands now split to __init__.

--- rplugin/python3/nvim_brojectile/__init__.py
+++ rplugin/python3/nvim_brojectile/__init__.py
@@ -1,8 +1,53 @@
-import neovim
+import pynvim
 from .brojectile import Brojectile
 
-@neovim.plugin
+@pynvim.plugin
 class BrojectileHandler(object):
     def __init__(self, nvim):
         self.nvim = nvim
         self.Brojectile = Brojectile(nvim)
+
+    @pynvim.command('BtileList', nargs='*', sync=False)
+    def read_Brojectile_bookmarks(self, args):
+        if len(args) == 1:
+            wrapcommand = "BtileCD {}".format(args[0])
+        elif len(args) == 0:
+            wrapcommand = 'BtileCD'
+        else:
+            self.Brojectile.error('BtileList takes 1 or 0 arguments, showing normal list')
+            wrapcommand = 'BtileCD'
+
+        self.nvim.async_call(self.Brojectile.fzf_call, wrapcommand)
+
+    @pynvim.command('BtileAdd', sync=False)
+    def add_Brojectile_pwd(self):
+        pwd = self.nvim.command_output('pwd')
+        self.Brojectile.add_bookmark(pwd)
+
+    @pynvim.command('BtileDel', sync=False)
+    def del_Brojectile_pwd(self):
+        self.nvim.async_call(self.Brojectile.fzf_call, 'BtileRM')
+
+    @pynvim.command('BtileRM', nargs=1, sync=True)
+    def delete_entry(self, args):
+        path = str(args).strip("[']").replace(' ', '').replace('\\', '')
+        self.Brojectile.delete_bookmark(path)
+
+    @pynvim.command('BtileCD', nargs='*', sync=False)
+    def change_directory(self, args):
+        if len(args) > 1:
+            path = args[1]
+            command = True
+        else:
+            path = args[0]
+            command = False
+
+        path = str(path).strip("[']").replace(' ', '').replace('\\', '')
+        self.nvim.chdir(path)
+        if command:
+            self.nvim.command(args[0])
+
+    @pynvim.function('Brojectile_list_bookmarks', sync=True)
+    def list_bookmarks(self, args):
+        self.Brojectile.read_bookmarks()
+        return self.Brojectile.bookmarks['bookmarks']
